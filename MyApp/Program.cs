edb6e7e3afbb0ece353cb6260f8817ba32954252
olivier1990 <olivierstordiau@gmail.com>
2022-10-13 19:07:40 +0200

    update

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -875,3 +875,169 @@ class Prog {
 }
 
 
+
+///
+// PENDU
+///
+
+
+
+// TO DELETE
+// // input utilisateur une lettre pour trouver le mot
+// Console.WriteLine("Entrer une lettre pour trouver le mot");
+// // user input
+// string userInput = Console.ReadLine();
+// // Print les variables 
+// Console.WriteLine("Username is: " + userInput + "\n"+ WordToGuess);
+
+
+// public class Hangman{
+//     static void WordRandomizer() 
+//     {
+//     // this.something
+    
+//     // string arrayWordToGuess = rand.arrayWordToGuess;
+//     // Console.WriteLine("I just got executed!");
+//     }
+
+
+
+//     static void Main(string[] args)
+//   {
+//     WordRandomizer();
+//     Car myObj = new Car();
+//     Console.WriteLine(myObj.color);
+//   }
+//   if (userInput = arrayWordToGuess)
+
+// }
+
+
+
+
+
+
+///////////////////
+/// PENDU WORKS///
+/////////////////
+
+// // Pour le REGEX
+// using System.Text.RegularExpressions;
+
+
+// // Array avec les mots a trouver
+// // string[] words = {"olivier", "michel", "seb"};
+// string[] words = {"olivier"};
+
+
+// // Nombres de vie
+// int lives = 5;
+// // Prendre un mot random de l'array words
+// string WordToGuess = words[new Random().Next(0, words.Length - 1)];
+
+ 
+
+
+// // TOFIX n'accepte pas encore les mots/noms avec majuscules
+// // Limite userInput au charactères  a-z
+// var validCharacters = new Regex("^[a-z]$");
+
+// // Collection qui contient les lettres envoyer du client userInput, s'appellera key plus tard
+// var letters = new List<string>();
+
+
+// // Tant qu'il y ait des vies on continue
+// while (lives != 0)
+// {
+//     // Lettres du mot qu'il reste a deviné.
+//     var charactersLeft = 0;
+    
+//     // Boucle chaque lettre du WordToGuess
+//     foreach (var character in WordToGuess)
+//     {
+//         // Lettre transformé en string
+//         var letter = character.ToString();
+        
+//         ///
+//         // Visuel du mot a trouver
+//         ///
+//         // On print la lettre si elle est dans l'array
+//         if (letters.Contains(letter))
+//         {
+//             Console.Write(letter);
+//         }
+//         else
+//         {
+//             // Les vides on ajoute _
+//             Console.Write("_");
+//             // On ajoute 1 par nombre de _ restant
+//             charactersLeft++;
+//         }
+//     }
+//     Console.WriteLine(string.Empty);
+
+//     // Arrête la boucle si il ne reste pas de charactères
+//     if (charactersLeft == 0)
+//     {
+//         break;
+//     }
+
+
+//     // input Client de la lettre
+//     Console.Write("Entrer une lettre ");
+
+//     // key contient la lettre input par le client convertit en minuscule (ToLower())
+//     // Console.ReadKey obtient prochain charactères ou fonction appuyer par l'utilisateur
+//     var key = Console.ReadKey().Key.ToString().ToLower();
+
+//     // Si on utilise pas ceci on a un doublon au niveau des lettres trouvés (liés au Console.WriteLine)
+//     Console.WriteLine(string.Empty);
+
+//     // Si elle ne respecte pas les règles regex 
+//     if (!validCharacters.IsMatch(key))
+//     {
+//         //  charactères invalide (chiffres etc..)
+//         Console.WriteLine($"\n\nLa lettre {key} est invalide. Réessayer.");
+//         // continue permet de continuer la boucle, break la stop
+//         continue;
+//     }
+
+//     // SI tu as déja entré cette lettre Message
+//     if (letters.Contains(key))
+//     {
+//         Console.WriteLine("\n\nVous avez déja entrer cette lettre");
+//         continue;
+//     }
+
+//     // Ajoute la lettre de l'input user si elle n'est pas encore intégrer
+//     letters.Add(key);
+
+//     // On enlève une vie si on se trompe de lettre
+//     // SI différent de WordToGuess alors
+//     if (!WordToGuess.Contains(key))
+//     {
+//         lives--;
+
+//         // Si il reste des vies on le montre
+//         if (lives > 0)
+//         {
+//             Console.WriteLine($"\n\nLa lettre {key} n'est pas dans le mot. Vous avez {lives} vie restante.");
+//         }
+//     }
+// }
+
+// // Messages lorsqu'on a gagné
+// if (lives > 0)
+// {
+//     // print message gagné + vie restante
+//     Console.WriteLine($"\n\nVous avez gagné avec {lives} vie restante!");
+// }
+// else
+// {
+//     // Message Perdu
+//     Console.WriteLine($"\n\nVous avez perdu le mot était '{WordToGuess}'.");   
+// }
+
+
+
+
